648d3c0ce7427666b158c33c43e9fcbfa5839f73
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2019-12-13 12:08:35 -0700

    .

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 # FactoCord start script
 # By Allen Lydiard (FM1337)
 
-rm /home/fact/fact-e/saves/*.tmp.*
-screen -dmS FactoCord-e ./FactoCord
+rm /home/fact/fact-g/saves/*.tmp.*
+screen -dmS FactoCord-g ./FactoCord
 echo "Hey if you're running this manually and not as a service, you can go ahead and just ctrl+c this script and the server will continue to run!"
-while screen -list | grep -q FactoCord-e
+while screen -list | grep -q FactoCord-g
 do
     sleep 30
 done
