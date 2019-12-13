277c8f9ba37df760bbfe89f18afcea37545d1c61
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2019-12-13 12:06:19 -0700

    git

--- copyall.sh
+++ copyall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-a/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-b/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-c/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-d/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-e/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-mod/FactoCord
-cp -f /home/fact/cord-e/cord-e /home/fact/cord-pvp/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-a/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-b/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-c/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-d/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-e/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-f/FactoCord
+cp -f /home/fact/cord-g/cord-g /home/fact/cord-g/FactoCord
