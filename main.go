af5c4ab056c73ce7ce7e30ede94dc49626b020a7
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-01-13 13:13:50 -0700

    test

--- main.go
+++ main.go
@@ -48,6 +48,8 @@ func main() {
 	mwriter := io.MultiWriter(logging, os.Stdout)
 	//Pre-init
 	cmd := exec.Command("/usr/bin/time", "")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = mwriter
 	glob.Pipe, err = cmd.StdinPipe()
 
 	start_bot()
@@ -116,8 +118,8 @@ func main() {
 					//support.Log(buf)
 				}
 				cmd := exec.Command(support.Config.Executable, support.Config.LaunchParameters...)
-				//cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-				//cmd.Stderr = os.Stderr
+				cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+				cmd.Stderr = os.Stderr
 				cmd.Stdout = mwriter
 				glob.Pipe, err = cmd.StdinPipe()
 				if err != nil {
