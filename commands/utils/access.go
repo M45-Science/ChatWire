9f7f7109b008ff86abda0261b612682b792e5306
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-02-24 02:15:49 -0700

    Update access.go

--- commands/utils/access.go
+++ commands/utils/access.go
@@ -15,9 +15,10 @@ func AccessServer(s *discordgo.Session, m *discordgo.MessageCreate) {
 	g := xkcdpwgen.NewGenerator()
 	g.SetNumWords(5)
 	g.SetCapitalize(true)
+	g.SetDelimiter("-")
 	password := g.GeneratePasswordString()
 
-	_, err := s.ChannelMessageSend(support.Config.FactorioChannelID, fmt.Sprintf("Access Code: `%s`\n \nPress ~ or to \` to access chat,\nthen type /access `%s`, and press enter.\nYou can do this on any of our factorio servers to be verified!\nYou can copy-paste (control-c, control-v) the code from discord and into factorio.", password, password))
+	_, err := s.ChannelMessageSend(support.Config.FactorioChannelID, fmt.Sprintf("Access Code: `%s`\n \nPress ~ or to \\` to access chat,\nthen type /access `%s`, and press enter.\nYou can do this on any of our factorio servers to be verified!\nYou can copy-paste (control-c, control-v) the code from discord and into factorio.", password, password))
 	if err != nil {
 		support.ErrorLog(err)
 	}
