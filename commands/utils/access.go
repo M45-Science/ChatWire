8b253c8e5956236adb186f7a96779e46514529d9
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-02-24 02:36:40 -0700

    change $access code to dm

--- commands/utils/access.go
+++ commands/utils/access.go
@@ -27,7 +27,8 @@ func AccessServer(s *discordgo.Session, m *discordgo.MessageCreate) {
 	glob.PasswordName[glob.PasswordPos] = m.Author.Username
 	glob.PasswordPos++
 
-	_, err := s.ChannelMessageSend(support.Config.FactorioChannelID, fmt.Sprintf("Access Code: `%s`\n \nPress ~ or to \\` to access chat,\nthen type `/access %s`, and press enter.\nYou can do this on any of our factorio servers to be verified!\nYou can copy-paste (control-c, control-v) the code from discord and into factorio.", password, password))
+	dmChannel, _ := session.UserChannelCreate(idOfUserToDM)
+	_, err := s.ChannelMessageSend(dmChannel.ID,, fmt.Sprintf("Access Code: `%s`\n \nPress ~ or to \\` to access chat,\nthen type `/access %s`, and press enter.\nYou can do this on any of our factorio servers to be verified!\nYou can copy-paste (control-c, control-v) the code from discord and into factorio.", password, password))
 	if err != nil {
 		support.ErrorLog(err)
 	}
