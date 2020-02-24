07856c53822f04c49eba0585f9433a2d2480447c
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-02-24 03:20:16 -0700

    $help with desc

--- commands/commands.go
+++ commands/commands.go
@@ -21,6 +21,7 @@ type Command struct {
 	Name    string
 	Command func(s *discordgo.Session, m *discordgo.MessageCreate)
 	Admin   bool
+	Help    string
 }
 
 // CL is a Commands interface.
@@ -29,22 +30,22 @@ var CL Commands
 // RegisterCommands registers the commands on start up.
 func RegisterCommands() {
 	// Admin Commands
-	CL.CommandList = append(CL.CommandList, Command{Name: "Stop", Command: admin.StopServer, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Restart", Command: admin.Restart, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Start", Command: admin.Restart, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Reload", Command: admin.Reload, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Reboot", Command: admin.Reboot, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Save", Command: admin.SaveServer, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Queue", Command: admin.Queue, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Rand", Command: admin.RandomMap, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Gen", Command: admin.Generate, Admin: true})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Stat", Command: admin.StatServer, Admin: true})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Stop", Command: admin.StopServer, Admin: true, Help: "Stops server"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Restart", Command: admin.Restart, Admin: true, Help: "Restarts server"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Start", Command: admin.Restart, Admin: true, Help: "Starts server"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Reload", Command: admin.Reload, Admin: true, Help: "Reloads bot & server"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Reboot", Command: admin.Reboot, Admin: true, Help: "Force restart bot"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Save", Command: admin.SaveServer, Admin: true, Help: "Tell server to save map"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Queue", Command: admin.Queue, Admin: true, Help: "Reloads bot when server is empty."})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Rand", Command: admin.RandomMap, Admin: true, Help: "Make a random map preview"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Gen", Command: admin.Generate, Admin: true, Help: "Makes a map from preview"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Stat", Command: admin.StatServer, Admin: true, Help: "Shows server stats"})
 
 	// Util Commands
-	CL.CommandList = append(CL.CommandList, Command{Name: "Online", Command: utils.PlayersOnline, Admin: false})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Mods", Command: utils.ModsList, Admin: false})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Access", Command: utils.AccessServer, Admin: false})
-	CL.CommandList = append(CL.CommandList, Command{Name: "Help", Command: Help, Admin: false})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Online", Command: utils.PlayersOnline, Admin: false, Help: "Show players online"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Mods", Command: utils.ModsList, Admin: false, Help: "Show installed mods"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Access", Command: utils.AccessServer, Admin: false, Help: "Get an access code to get regular status on discord (WIP"})
+	CL.CommandList = append(CL.CommandList, Command{Name: "Help", Command: Help, Admin: false, Help: "You are here"})
 }
 
 // RunCommand runs a specified command.
@@ -81,9 +82,9 @@ func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	for _, command := range CL.CommandList {
 		admin := ""
 		if command.Admin {
-			admin = "(Admin)"
+			admin = "(Admin-Only)"
 		}
-		buf = buf + fmt.Sprintf("%s%-18s %s\n", support.Config.Prefix, strings.ToLower(command.Name), admin)
+		buf = buf + fmt.Sprintf("%s%-18s %s %s\n", support.Config.Prefix, strings.ToLower(command.Name), admin, command.Help)
 	}
 	buf = buf + "\n```"
 
