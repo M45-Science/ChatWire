ae3841b1ca7b17d2483a960f0b86a7a10da90727
Distortions81 <carlotto81@gmail.com>
2020-11-28 09:07:03 -0700

    Check if update is configured

--- commands/admin/update.go
+++ commands/admin/update.go
@@ -1,10 +1,15 @@
 package admin
 
 import (
+	"../../config"
 	"../../fact"
 	"github.com/bwmarrin/discordgo"
 )
 
 func Update(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	fact.CheckFactUpdate(true)
+	if config.Config.UpdaterPath != "" {
+		fact.CheckFactUpdate(true)
+	} else {
+		fact.CMS(m.ChannelID, "Updater is not configured.")
+	}
 }
