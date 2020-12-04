30e21e5c0b44f7ba5b5cfa1a3cbad775c4d91d2b
Distortions81 <carlotto81@gmail.com>
2020-12-03 17:01:08 -0700

    add cancel option

--- commands/admin/update.go
+++ commands/admin/update.go
@@ -1,13 +1,23 @@
 package admin
 
 import (
+	"strings"
+
 	"../../config"
 	"../../fact"
 	"github.com/bwmarrin/discordgo"
 )
 
 func Update(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	argnum := len(args)
+
 	if config.Config.UpdaterPath != "" {
+		if argnum > 0 && strings.ToLower(args[0]) == "cancel" {
+			fact.SetDoUpdateFactorio(false)
+			config.Config.UpdaterPath = ""
+			fact.CMS(m.ChannelID, "Update canceled, and update check disabled.")
+			return
+		}
 		fact.CheckFactUpdate(true)
 	} else {
 		fact.CMS(m.ChannelID, "Updater is not configured.")
