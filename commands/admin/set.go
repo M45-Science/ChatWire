bdd3e43440da6ba117a2121f2b6f90788fe7091d
Distortions <33408758+Distortions81@users.noreply.github.com>
2021-09-30 01:01:26 -0600

    argnum

--- commands/admin/set.go
+++ commands/admin/set.go
@@ -22,14 +22,11 @@ func handlebool(name string, arg string, m *discordgo.MessageCreate) (bool, bool
 }
 
 func Set(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	arglen := len(args)
-	if arglen > 0 {
-		arg1 := strings.ToLower(args[0])
-		arg2 := ""
 
-		if arglen > 1 {
-			arg2 = strings.ToLower(args[1])
-		}
+	arglen := len(args)
+	if arglen > 1 {
+		arg1 := strings.ToLower(args[0])
+		arg2 := strings.ToLower(args[1])
 
 		if arg1 == "name" && arg2 != "" {
 			cfg.Local.Name = arg2
