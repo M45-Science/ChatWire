baa4b50de6e4229dbd74eb4b59070720a2c007ea
distortions81 <distortions81@gmail.com>
2021-12-21 18:37:14 -0700

    custom logger

---
+++ botlog/botlog.go
@@ -0,0 +1,18 @@
+package botlog
+
+import (
+	"ChatWire/glob"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+func DoLog(text string) {
+	ctime := time.Now()
+	_, filename, line, _ := runtime.Caller(1)
+
+	date := fmt.Sprintf("%2v:%2v.%2v", ctime.Hour(), ctime.Minute(), ctime.Second())
+	buf := fmt.Sprintf("%v: %15v:%5v: %v\n", date, filepath.Base(filename), line, text)
+	glob.BotLogDesc.WriteString(buf)
+}
