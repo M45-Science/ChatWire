589f1611460948ef0fd9679a3d4d90b4d70f7114
distortions81 <distortions81@gmail.com>
2021-12-25 23:52:09 -0700

    more info

--- commands/user/info.go
+++ commands/user/info.go
@@ -38,22 +38,22 @@ func Info(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 	tnow = tnow.Round(time.Second)
 
 	buf := "```"
-	buf = buf + fmt.Sprintf("ChatWire    : %v\n", constants.Version)
-	buf = buf + fmt.Sprintf("Factorio    : %v\n", glob.FactorioVersion)
-	buf = buf + fmt.Sprintf("Last Reboot : %v\n", tnow.Sub(glob.Uptime.Round(time.Second)).String())
-	buf = buf + fmt.Sprintf("Map Time    : %v\n", glob.GametimeString)
-	buf = buf + fmt.Sprintf("Players     : %v (Record %v)\n", fact.GetNumPlayers(), glob.RecordPlayers)
-	buf = buf + fmt.Sprintf("Members     : %v\n", nummember)
-	buf = buf + fmt.Sprintf("Regulars    : %v\n", numregulars)
-	buf = buf + fmt.Sprintf("Registered  : %v\n", numreg)
+	buf = buf + fmt.Sprintf("ChatWire       : %v\n", constants.Version)
+	buf = buf + fmt.Sprintf("Factorio       : %v\n", glob.FactorioVersion)
+	buf = buf + fmt.Sprintf("Last Reboot    : %v\n", tnow.Sub(glob.Uptime.Round(time.Second)).String())
+	buf = buf + fmt.Sprintf("Map Time       : %v\n", glob.GametimeString)
+	buf = buf + fmt.Sprintf("Players        : %v (Record %v)\n", fact.GetNumPlayers(), glob.RecordPlayers)
+	buf = buf + fmt.Sprintf("Members        : %v\n", nummember)
+	buf = buf + fmt.Sprintf("Regulars       : %v\n", numregulars)
+	buf = buf + fmt.Sprintf("Registered     : %v\n", numreg)
 
 	if cfg.Local.MapGenPreset != "" {
 		buf = buf + fmt.Sprintf("Map-Gen Preset : %v\n", cfg.Local.MapGenPreset)
 	}
 	if cfg.Local.ResetScheduleText != "" {
-		buf = buf + fmt.Sprintf("Reset Schedule  : %v\n", cfg.Local.ResetScheduleText)
+		buf = buf + fmt.Sprintf("Reset Schedule : %v\n", cfg.Local.ResetScheduleText)
 	}
-	buf = buf + fmt.Sprintf("Slow Connect    : %v\n", support.BoolToString(cfg.Local.SlowConnect.SlowConnect))
+	buf = buf + fmt.Sprintf("Slow Connect   : %v\n", support.BoolToString(cfg.Local.SlowConnect.SlowConnect))
 	buf = buf + "```"
 	fact.CMS(m.ChannelID, buf)
 
