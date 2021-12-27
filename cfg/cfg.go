c98f280e4d341119ae7acff59ab452593dc4972a
Distortions81 <carlotto81@gmail.com>
2021-12-27 01:09:31 -0700

    automatic basic config on no config file

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -2,9 +2,13 @@ package cfg
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
+	"math"
+	"math/rand"
 	"os"
+	"path/filepath"
 
 	"ChatWire/botlog"
 	"ChatWire/constants"
@@ -198,30 +202,142 @@ func WriteGCfg() bool {
 	return true
 }
 
+func randomBase64String(l int) string {
+	buff := make([]byte, int(math.Ceil(float64(l)/float64(1.33333333333))))
+	rand.Read(buff)
+	str := base64.RawURLEncoding.EncodeToString(buff)
+	return str[:l] // strip 1 extra character we get from odd length results
+}
+
 func ReadGCfg() bool {
 
 	_, err := os.Stat(constants.CWGlobalConfig)
 	notfound := os.IsNotExist(err)
 
 	if notfound {
-		botlog.DoLog("ReadGCfg: os.Stat failed")
-		return false
+		botlog.DoLog("ReadGCfg: os.Stat failed, auto-defaults generated.")
+		newcfg := CreateGCfg()
+		Global = newcfg
 
+		if Global.Domain == "" {
+			Global.Domain = "private"
+		}
+		if Global.RconPortOffset == 0 {
+			Global.RconPortOffset = 10000
+		}
+		if Global.RconPass == "" {
+			Global.RconPass = randomBase64String(64)
+			botlog.DoLog("No RCON password specified. Random one generated.")
+		}
+		if Global.GroupName == "" {
+			Global.GroupName = randomBase64String(3)
+			botlog.DoLog("No group name specified. Random one generated.")
+		}
+		if Global.DiscordCommandPrefix == "" {
+			Global.DiscordCommandPrefix = "$"
+		}
+
+		if Global.PathData.FactorioServersRoot == "" {
+			ex, err := os.Executable()
+			if err == nil {
+				exPath := filepath.Dir(ex)
+				p := filepath.Clean(filepath.Join(exPath, ".."))
+				Global.PathData.FactorioServersRoot = p
+			}
+		}
+
+		//Automatic config
+		if Global.PathData.FactorioHomePrefix == "" {
+			Global.PathData.FactorioHomePrefix = "fact-"
+		}
+		if Global.PathData.ChatWireHomePrefix == "" {
+			Global.PathData.ChatWireHomePrefix = "cw-"
+		}
+		if Global.PathData.RecordPlayersFilename == "" {
+			Global.PathData.RecordPlayersFilename = "most-player.dat"
+		}
+		if Global.PathData.SaveFilePath == "" {
+			Global.PathData.SaveFilePath = "saves"
+		}
+		if Global.PathData.DBFileName == "" {
+			Global.PathData.DBFileName = "playerdb.dat"
+			_, err := os.Create(Global.PathData.DBFileName)
+			if err != nil {
+				botlog.DoLog("Could not create playerdb.dat")
+				return false
+			}
+		}
+		if Global.PathData.FactUpdateCache == "" {
+			Global.PathData.FactUpdateCache = Global.PathData.FactorioServersRoot + "/update-cache/"
+		}
+		if Global.PathData.MapPreviewPath == "" {
+			Global.PathData.MapPreviewPath = Global.PathData.FactorioServersRoot + "/public_html/map-preview/"
+		}
+		if Global.PathData.MapArchivePath == "" {
+			Global.PathData.MapArchivePath = Global.PathData.FactorioServersRoot + "/public_html/archive/"
+		}
+		if Global.PathData.MapPreviewURL == "" {
+			Global.PathData.MapPreviewURL = "http://" + Global.Domain + "/~username/map-preview/"
+		}
+		if Global.PathData.ArchiveURL == "" {
+			Global.PathData.ArchiveURL = "http://" + Global.Domain + "/~username/archive/"
+		}
+		if Global.PathData.ImageMagickPath == "" {
+			Global.PathData.ImageMagickPath = "/usr/bin/convert"
+		}
+		if Global.PathData.RMPath == "" {
+			Global.PathData.RMPath = "/bin/rm"
+		}
+		if Global.PathData.ShellPath == "" {
+			Global.PathData.ShellPath = "/bin/bash"
+		}
+		if Global.PathData.MapGenPath == "" {
+			Global.PathData.MapGenPath = "map-gen-json"
+
+			err := os.MkdirAll(Global.PathData.FactorioServersRoot+"/"+Global.PathData.MapGenPath, os.ModePerm)
+			if err != nil {
+				botlog.DoLog("Could not create map-gen-json directory.")
+				return false
+			}
+		}
+		if Global.PathData.ZipBinaryPath == "" {
+			Global.PathData.ZipBinaryPath = "/usr/bin/unzip"
+		}
+		if Global.PathData.FactorioBinary == "" {
+			Global.PathData.FactorioBinary = "bin/x64/factorio"
+		}
+		if Global.DiscordData.GuildID == "" {
+			botlog.DoLog("No Discord Guild ID specified. This MUST be set!")
+			Global.DiscordData.GuildID = "MY DISCORD GUILD/SERVER ID"
+		}
+		if Global.DiscordData.Token == "" {
+			botlog.DoLog("No Discord Token specified. This MUST be set!")
+			Global.DiscordData.Token = "MY DISCORD BOT TOKEN"
+		}
+		if Global.FactorioData.Username == "" {
+			botlog.DoLog("No Factorio Username specified. This MUST be set!")
+			Global.FactorioData.Username = "MY FACTORIO USERNAME"
+		}
+		if Global.FactorioData.Token == "" {
+			botlog.DoLog("No Factorio Token specified. This MUST be set!")
+			Global.FactorioData.Token = "MY FACTORIO TOKEN"
+		}
+		return true
 	} else {
 
 		file, err := ioutil.ReadFile(constants.CWGlobalConfig)
 
 		if file != nil && err == nil {
-			cfg := CreateGCfg()
+			newcfg := CreateGCfg()
 
-			err := json.Unmarshal([]byte(file), &cfg)
+			err := json.Unmarshal([]byte(file), &newcfg)
 			if err != nil {
 				botlog.DoLog("ReadGCfg: Unmashal failure")
 				botlog.DoLog(err.Error())
 				os.Exit(1)
 			}
 
-			Global = cfg
+			Global = newcfg
 
 			return true
 		} else {
@@ -232,8 +348,8 @@ func ReadGCfg() bool {
 }
 
 func CreateGCfg() gconfig {
-	cfg := gconfig{Version: "0.0.1"}
-	return cfg
+	newcfg := gconfig{Version: "0.0.1"}
+	return newcfg
 }
 
 func WriteLCfg() bool {
@@ -280,34 +396,63 @@ func ReadLCfg() bool {
 	notfound := os.IsNotExist(err)
 
 	if notfound {
-		botlog.DoLog("ReadLCfg: os.Stat failed")
-		return false
+		botlog.DoLog("ReadLCfg: os.Stat failed, auto-defaults generated.")
+		newcfg := CreateLCfg()
+		Local = newcfg
 
+		if Local.Name == "" {
+			Local.Name = "unnamed"
+		}
+		if Local.ServerCallsign == "" {
+			Local.ServerCallsign = "a"
+		}
+		if Local.Port <= 0 {
+			Local.Port = 34197
+		}
+		if Local.ChannelData.ChatID == "" {
+			botlog.DoLog("ReadLCfg: ChatID not set, thist MUST be set to a valid Discord channel ID!")
+			Local.ChannelData.ChatID = "MY DISCORD CHANNEL ID"
+		}
+		WriteLCfg()
+		return true
 	} else {
 
 		file, err := ioutil.ReadFile(constants.CWLocalConfig)
 
 		if file != nil && err == nil {
-			cfg := CreateLCfg()
+			newcfg := CreateLCfg()
 
-			err := json.Unmarshal([]byte(file), &cfg)
+			err := json.Unmarshal([]byte(file), &newcfg)
 			if err != nil {
 				botlog.DoLog("ReadLCfg: Unmashal failure")
 				botlog.DoLog(err.Error())
 				os.Exit(1)
 			}
 
-			Local = cfg
+			Local = newcfg
+
+			//Automatic config
+			found := false
+			for _, t := range constants.MapTypes {
+				if Local.MapPreset == t {
+					found = true
+				}
+			}
+			if !found {
+				Local.MapPreset = constants.MapTypes[1]
+				botlog.DoLog("ReadLCfg: MapPreset not valid, setting to " + Local.MapPreset)
+			}
 
 			return true
 		} else {
 			botlog.DoLog("ReadLCfg: ReadFile failure")
 			return false
 		}
+
 	}
 }
 
 func CreateLCfg() config {
-	cfg := config{Version: "0.0.1"}
-	return cfg
+	newcfg := config{Version: "0.0.1"}
+	return newcfg
 }
