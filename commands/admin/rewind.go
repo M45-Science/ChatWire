afac5880243d281aa7ed504144ed8e60a77126a0
Distortions81 <carlotto81@gmail.com>
2021-12-27 15:03:24 -0700

    better autosave listing

--- commands/admin/rewind.go
+++ commands/admin/rewind.go
@@ -2,9 +2,11 @@ package admin
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -17,11 +19,14 @@ import (
 
 func Rewind(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 
+	layoutUS := "01/02 03:04 PM"
 	argnum := len(args)
 	path := cfg.Global.PathData.FactorioServersRoot + cfg.Global.PathData.FactorioHomePrefix + cfg.Local.ServerCallsign + "/" + cfg.Global.PathData.SaveFilePath
 
+	//Correct number of arguments (1)
 	if argnum == 1 {
 		num, err := strconv.Atoi(args[0])
+		//Seems to be a number
 		if err == nil {
 			if num < 0 || num > 9999 {
 				fact.CMS(m.ChannelID, "That isn't an acceptable number.")
@@ -53,41 +58,85 @@ func Rewind(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 
 				}
 			}
+			//Not a number
 		} else {
+			//List all autosaves
 			if strings.EqualFold(args[0], "list") {
 				files, err := ioutil.ReadDir(path)
+				//We can't read saves dir
 				if err != nil {
 					log.Fatal(err)
 					fact.CMS(m.ChannelID, "Something went wrong, sorry.")
 				}
 
-				buf := ""
+				rangeBuf := ""
+				fileNames := ""
 				lastNum := -1
+				step := 2
+				//Loop all files
+				var tempf []fs.FileInfo
 				for _, f := range files {
+					tempf = append(tempf, f)
+				}
+
+				sort.Slice(tempf, func(i, j int) bool {
+					return tempf[i].ModTime().Before(tempf[j].ModTime())
+				})
+
+				maxList := 20
+				for _, f := range tempf {
+					maxList--
+					if maxList <= 0 {
+						break
+					}
 					fName := f.Name()
+
+					//Check if its a properly name autosave
 					if strings.HasPrefix(fName, "_autosave") && strings.HasSuffix(fName, ".zip") {
 						fTmp := strings.TrimPrefix(fName, "_autosave")
 						fNumStr := strings.TrimSuffix(fTmp, ".zip")
-						fNum, err := strconv.Atoi(fNumStr)
+						fNum, err := strconv.Atoi(fNumStr) //autosave number
+						//Nope, no valid numer
 						if err != nil {
 							continue
 						}
-						if fNum != lastNum+1 {
-							if buf == "" {
-								buf = "Autosaves: "
+						step++
+
+						//Get mod date
+						modDate := f.ModTime().Local().Format(layoutUS)
+						//Not first file add commas/newlines
+						if fileNames != "" {
+							if step%3 == 0 {
+								fileNames = fileNames + "\n"
 							} else {
-								buf = buf + "-" + strconv.Itoa(lastNum) + ", "
+								fileNames = fileNames + ", "
 							}
-							buf = buf + fmt.Sprintf("%v", fNum)
-						} else {
-							//Silent
 						}
-						lastNum = fNum
+						//Add to list with mod date
+						fileNames = fileNames + fmt.Sprintf("(%15v ): #%3v", modDate, fNum)
+
+						//autosave number range list
+						//If number is not sequential, save end of range and print it
+						if fNum != lastNum+1 {
+							//If we just started, add prefix, otherwise add dash and the end of the range, with comma for next item.
+							if rangeBuf == "" {
+								rangeBuf = "Autosaves: "
+							} else {
+								rangeBuf = rangeBuf + "-" + strconv.Itoa(lastNum) + ", "
+							}
+							rangeBuf = rangeBuf + fmt.Sprintf("%v", fNum)
+						}
+						lastNum = fNum //Save for compairsion next loop
 					}
 				}
-				buf = buf + "-" + strconv.Itoa(lastNum)
+				//Add last item to range
+				rangeBuf = rangeBuf + "-" + strconv.Itoa(lastNum)
 
-				fact.CMS(m.ChannelID, buf)
+				if lastNum == -1 {
+					fact.CMS(m.ChannelID, "No autosaves found.")
+				} else {
+					fact.CMS(m.ChannelID, "```\n"+rangeBuf+"\n\n"+fileNames+"\n```")
+				}
 				return
 			}
 			fact.CMS(m.ChannelID, "I didn't find a valid number, or `list`.")
