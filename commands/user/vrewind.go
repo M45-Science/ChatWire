fcdb526334844049774c23c38f99998dea13d543
distortions81 <distortions81@gmail.com>
2021-12-31 21:43:49 -0700

    start vote-rewind

---
+++ commands/user/vrewind.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"ChatWire/cfg"
+	"ChatWire/fact"
+	"ChatWire/glob"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func VoteReind(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+
+	//layoutUS := "01/02 03:04 PM MST"
+	argnum := len(args)
+	path := cfg.Global.PathData.FactorioServersRoot + cfg.Global.PathData.FactorioHomePrefix + cfg.Local.ServerCallsign + "/" + cfg.Global.PathData.SaveFilePath
+
+	for _, player := range glob.PlayerList {
+		if player.ID != "" && player.Level == 2 && player.ID == m.Author.ID {
+			//vote
+		} else {
+			fact.CMS(m.ChannelID, "You must registered, and a regular to use this command.")
+			return
+		}
+	}
+	//Correct number of arguments (1)
+	if argnum == 1 {
+		num, err := strconv.Atoi(args[0])
+		//Seems to be a number
+		if err == nil {
+			if num > 0 || num < 9999 {
+				//Check if file is valid and found
+				autoSaveStr := fmt.Sprintf("_autosave%v.zip", num)
+				_, err := os.Stat(path + "/" + autoSaveStr)
+				notfound := os.IsNotExist(err)
+
+				if notfound {
+					fact.CMS(m.ChannelID, "I don't see that autosave.")
+					return
+				} else {
+					//add vote
+				}
+			}
+		}
+	}
+
+}
