3157244e4639ab6467b195a2c7ae7c94c6a16fdc
Distortions81 <carlotto81@gmail.com>
2022-01-03 23:53:56 -0700

    Dynamically generated 'info' output

--- commands/user/info.go
+++ commands/user/info.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"ChatWire/cfg"
+	"ChatWire/commands/admin"
 	"ChatWire/constants"
 	"ChatWire/fact"
 	"ChatWire/glob"
@@ -15,6 +16,7 @@ import (
 
 //Show useful info about a server and it's settings
 func ShowSettings(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+
 	verbose := false
 	if len(args) > 0 && strings.EqualFold(args[0], "verbose") {
 		verbose = true
@@ -23,102 +25,49 @@ func ShowSettings(s *discordgo.Session, m *discordgo.MessageCreate, args []strin
 	buf := "```"
 	/* STATS */
 	buf = buf + "Stats:\n"
-	buf = buf + fmt.Sprintf("%25v: %v-%v\n", "Server name", cfg.Local.ServerCallsign, cfg.Local.Name)
-	buf = buf + fmt.Sprintf("%25v: m45sci.xyz:%v\n", "Address", cfg.Local.Port)
-	buf = buf + fmt.Sprintf("%25v: %v\n", "ChatWire version", constants.Version)
-	buf = buf + fmt.Sprintf("%25v: %v\n", "Factorio version", fact.FactorioVersion)
+	buf = buf + fmt.Sprintf("%17v: %v\n", "ChatWire version", constants.Version)
+	buf = buf + fmt.Sprintf("%17v: %v\n", "Factorio version", fact.FactorioVersion)
 	tnow := time.Now()
 	tnow = tnow.Round(time.Second)
-	buf = buf + fmt.Sprintf("%25v: %v\n", "ChatWire up-time", tnow.Sub(glob.Uptime.Round(time.Second)).String())
+	buf = buf + fmt.Sprintf("%17v: %v\n", "ChatWire up-time", tnow.Sub(glob.Uptime.Round(time.Second)).String())
 	if !fact.FactorioBootedAt.IsZero() && fact.IsFactorioBooted() {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Factorio up-time", tnow.Sub(fact.FactorioBootedAt.Round(time.Second)).String())
+		buf = buf + fmt.Sprintf("%17v: %v\n", "Factorio up-time", tnow.Sub(fact.FactorioBootedAt.Round(time.Second)).String())
 	} else {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Factorio up-time", "not running")
+		buf = buf + fmt.Sprintf("%17v: %v\n", "Factorio up-time", "not running")
 	}
-	numreg := 0
-	nummember := 0
-	numregulars := 0
 
-	glob.PlayerListLock.RLock()
-	for _, player := range glob.PlayerList {
-		if player.ID != "" {
-			numreg++
-		}
-		if player.Level == 1 {
-			nummember++
-		} else if player.Level == 2 {
-			numregulars++
-		}
-	}
-	glob.PlayerListLock.RUnlock()
 	if fact.LastSaveName != constants.Unknown || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Save name", fact.LastSaveName)
+		buf = buf + fmt.Sprintf("%17v: %v\n", "Save name", fact.LastSaveName)
 	}
-	buf = buf + fmt.Sprintf("%25v: %v\n", "Map time", fact.GametimeString)
-	buf = buf + fmt.Sprintf("%25v: %v (most ever %v)\n", "Players online", fact.GetNumPlayers(), glob.RecordPlayers)
+	buf = buf + fmt.Sprintf("%17v: %v\n", "Map time", fact.GametimeString)
+	buf = buf + fmt.Sprintf("%17v: %v (most ever %v)\n", "Players online", fact.GetNumPlayers(), glob.RecordPlayers)
 
 	/* SETTINGS */
-	buf = buf + "\nSettings:\n"
-	buf = buf + fmt.Sprintf("%25v: %v\n", "Map preset", cfg.Local.MapPreset)
-	if cfg.Local.MapGenPreset != "" || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Map gen", cfg.Local.MapGenPreset)
-	} else {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Map gen", "not set")
+	buf = buf + "\nServer settings:\n"
+	for _, item := range admin.SettingList {
+		if item.Type == admin.TYPE_STRING {
+			if *item.SData != "" || verbose {
+				buf = buf + fmt.Sprintf("%23v: %v\n", item.Desc, *item.SData)
+			}
+		} else if item.Type == admin.TYPE_INT {
+			if (*item.IData != 0 && *item.IData != item.DefInt) || verbose {
+				buf = buf + fmt.Sprintf("%23v: %v\n", item.Desc, *item.IData)
+			}
+		} else if item.Type == admin.TYPE_BOOL {
+			if *item.BData != item.DefBool || verbose {
+				buf = buf + fmt.Sprintf("%23v: %v\n", item.Desc, support.BoolToString(*item.BData))
+			}
+		} else if item.Type == admin.TYPE_F32 {
+			if (*item.FData32 != 0 && *item.FData32 != item.DefF32) || verbose {
+				buf = buf + fmt.Sprintf("%23v: %v\n", item.Desc, *item.FData32)
+			}
+		} else if item.Type == admin.TYPE_F64 {
+			if (*item.FData64 != 0 && *item.FData64 != item.DefF64) || verbose {
+				buf = buf + fmt.Sprintf("%23v: %v\n", item.Desc, *item.FData64)
+			}
+		}
 	}
-	if !cfg.Local.AutoStart || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Auto-start", support.BoolToString(cfg.Local.AutoStart))
-	}
-	if !cfg.Local.AutoUpdate || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Auto-update", support.BoolToString(cfg.Local.AutoUpdate))
-	}
-	if cfg.Local.UpdateFactExp || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Experimental-updates", cfg.Local.UpdateFactExp)
-	}
-	if cfg.Local.ResetScheduleText != "" || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Reset schedule", cfg.Local.ResetScheduleText)
-	}
-	if cfg.Local.DisableBlueprints || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Disable blueprints", support.BoolToString(cfg.Local.DisableBlueprints))
-	}
-	if cfg.Local.EnableCheats || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Enable cheats", support.BoolToString(cfg.Local.EnableCheats))
-	}
-	if cfg.Local.HideAutosaves || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Hide autosaves", support.BoolToString(cfg.Local.HideAutosaves))
-	}
-	if cfg.Local.SlowConnect.SlowConnect || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Slow connect", support.BoolToString(cfg.Local.SlowConnect.SlowConnect))
-	}
-
-	//Softmod
-	if cfg.Local.SoftModOptions.CleanMapOnBoot || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Clean map on boot", support.BoolToString(cfg.Local.SoftModOptions.CleanMapOnBoot))
-	}
-	if cfg.Local.SoftModOptions.DoWhitelist || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Members Only", support.BoolToString(cfg.Local.SoftModOptions.DoWhitelist))
-	}
-	if cfg.Local.SoftModOptions.FriendlyFire || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Friendly Fire", support.BoolToString(cfg.Local.SoftModOptions.FriendlyFire))
-	}
-	if !cfg.Local.SoftModOptions.RestrictMode || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "New Player Restrictions", support.BoolToString(cfg.Local.SoftModOptions.RestrictMode))
-	}
-	if !cfg.Local.FactorioData.AutoPause || verbose {
-		buf = buf + fmt.Sprintf("%25v: %v\n", "Auto Pause", support.BoolToString(cfg.Local.FactorioData.AutoPause))
-	}
-	if cfg.Local.FactorioData.AutoSaveMinutes < 10 || cfg.Local.FactorioData.AutoSaveMinutes > 15 || verbose {
-		buf = buf + fmt.Sprintf("%25v: %vm\n", "Autosaves", cfg.Local.FactorioData.AutoSaveMinutes)
-	}
-
-	if fact.ModLoadString != constants.Unknown {
-		buf = buf + "\nMod list: " + fact.ModLoadString + "\n"
-	}
-
-	buf = buf + "\n```"
-
-	if !verbose {
-		buf = buf + "More info: `$info verbose`\n"
-	}
+	buf = buf + "```For more info `" + cfg.Global.DiscordCommandPrefix + "info verbose`"
 	fact.CMS(m.ChannelID, buf)
 
 }
