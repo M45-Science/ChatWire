a462ba65835de799be2553a93fede305e832607a
Distortions81 <carlotto81@gmail.com>
2022-01-08 05:29:22 -0700

    async save votes

--- support/mainloops.go
+++ support/mainloops.go
@@ -412,6 +412,25 @@ func MainLoops() {
 			}
 		}()
 
+		//***************************************
+		//Save vote-rewind data async
+		//***************************************
+		go func() {
+
+			for glob.ServerRunning {
+				time.Sleep(5 * time.Second)
+
+				glob.VoteBoxLock.Lock()
+
+				//Save if dirty
+				if glob.VoteBox.Dirty {
+					fact.WriteRewindVotes()
+					glob.VoteBox.Dirty = false
+				}
+				glob.VoteBoxLock.Unlock()
+			}
+		}()
+
 		//****************************************************
 		//Slow-connect, helps players catch up on large maps
 		//****************************************************
