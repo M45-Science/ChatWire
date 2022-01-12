1ce0772150e325adf01687f525fc6bc239ff0a22
distortions81 <distortions81@gmail.com>
2022-01-11 18:27:18 -0700

    cooldown, void, write

--- commands/admin/rewind.go
+++ commands/admin/rewind.go
@@ -2,6 +2,8 @@ package admin
 
 import (
 	"ChatWire/fact"
+	"ChatWire/glob"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -13,7 +15,13 @@ func Rewind(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 
 	//Correct number of arguments (1)
 	if argnum == 1 {
-		fact.VoidAllVotes()
+
+		glob.VoteBoxLock.Lock()
+		glob.VoteBox.LastRewindTime = time.Now()
+		fact.VoidAllVotes()     //Void all votes
+		fact.WriteRewindVotes() //Save to file before exiting
+		glob.VoteBoxLock.Unlock()
+
 		fact.DoRewindMap(s, m, args[0])
 	} else {
 		fact.ShowRewindList(s, m)
