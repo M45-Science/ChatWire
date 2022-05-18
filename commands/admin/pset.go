6faf30033407014991e5e4d9b302e846c20e8798
Distortions81 <carlotto81@gmail.com>
2022-05-17 18:04:52 -0600

    Working

--- commands/admin/pset.go
+++ commands/admin/pset.go
@@ -9,64 +9,38 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-/* Set a player's level. Needs to support level names, and have useful help/errors */
+/* Set a player's level */
 func SetPlayerLevel(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	var aname string
 	var alevel int
 
 	a := i.ApplicationCommandData()
-	if !disc.CheckModerator(i.Member.Roles) {
-		buf := "This commands is only for moderators."
-		embed := &discordgo.MessageEmbed{Title: "Error:", Description: buf}
-		disc.InteractionResponse(s, i, embed)
-		return
-	}
 
+	//Get args
 	for _, arg := range a.Options {
 		if arg.Type == discordgo.ApplicationCommandOptionString {
-			if arg.StringValue() == "Moderator" {
-				aname = arg.StringValue()
-			}
+			aname = arg.StringValue()
 		} else if arg.Type == discordgo.ApplicationCommandOptionInteger {
 			alevel = int(arg.IntValue())
 		}
 	}
 
+	//Only if we have a name
 	if aname != "" {
 
 		oldLevel := fact.PlayerLevelGet(aname, true)
 
-		plevel := 0
-		if alevel == 255 {
-			plevel = 255
-		} else if alevel == 2 {
-			plevel = 2
-		} else if alevel == 1 {
-			plevel = 1
-		} else if alevel == 0 {
-			plevel = 0
-		} else if alevel == -1 {
-			plevel = -1
-		} else if alevel == -255 {
-			plevel = -255
-		} else {
-			buf := "Invalid level."
-			embed := &discordgo.MessageEmbed{Title: "Error:", Description: buf}
-			disc.InteractionResponse(s, i, embed)
-			return
-		}
-
-		/* Unban automatically */
-		if plevel >= 0 && oldLevel == -1 {
-			fact.WriteFact("/unban " + aname)
-		}
-
 		/* TRUE, modify only */
-		if fact.PlayerLevelSet(aname, plevel, true) {
+		if fact.PlayerLevelSet(aname, alevel, true) {
+			/* Unban automatically */
+			if alevel >= 0 && oldLevel == -1 {
+				fact.WriteFact("/unban " + aname)
+			}
+
 			fact.AutoPromote(aname)
 			fact.SetPlayerListDirty()
-			buf := fmt.Sprintf("Player: %v level set to %v", aname, fact.LevelToString(plevel))
+			buf := fmt.Sprintf("Player: %v level set to %v", aname, fact.LevelToString(alevel))
 			embed := &discordgo.MessageEmbed{Title: "Complete:", Description: buf}
 			disc.InteractionResponse(s, i, embed)
 			return
@@ -76,10 +50,6 @@ func SetPlayerLevel(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			disc.InteractionResponse(s, i, embed)
 			return
 		}
-	} else {
-		buf := "Invalid level.\nValid levels are:\n`Admin, Regular, Member, New`. Also: `Banned` and `Deleted`."
-		embed := &discordgo.MessageEmbed{Title: "Error:", Description: buf}
-		disc.InteractionResponse(s, i, embed)
 	}
 
 }
