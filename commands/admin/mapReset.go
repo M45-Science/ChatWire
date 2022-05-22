b485e4120409366da3bd8cca88f3b615ce87af4e
Distortions81 <carlotto81@gmail.com>
2022-05-22 10:31:44 -0600

    Show reponse to /archive

--- commands/admin/mapReset.go
+++ commands/admin/mapReset.go
@@ -3,10 +3,12 @@ package admin
 import (
 	"github.com/bwmarrin/discordgo"
 
+	"ChatWire/disc"
 	"ChatWire/fact"
 )
 
 /* Generate map */
 func MapReset(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	fact.Map_reset("", true)
+	disc.EphemeralResponse(s, i, "Status:", "Resetting map...")
 }
