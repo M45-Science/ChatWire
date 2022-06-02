14ebe934384596d9a30c8b2dd1074cc09fb8bb91
Distortions81 <carlotto81@gmail.com>
2022-06-01 19:25:40 -0600

    Complete new mod ChangeMap command

--- commands/moderator/newMapChange.go
+++ commands/moderator/newMapChange.go
@@ -1,12 +1,10 @@
 package moderator
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 
-	"ChatWire/cfg"
 	"ChatWire/fact"
 	"ChatWire/glob"
 )
@@ -14,20 +12,11 @@ import (
 /* Load a different save-game */
 func ChangeMap(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
-	var autosaveNum int64 = -1
-
-	/* Correct number of arguments (1) */
-	if autosaveNum > 0 && autosaveNum <= int64(cfg.Global.Options.AutosaveMax) {
-
-		glob.VoteBoxLock.Lock()
-		glob.VoteBox.LastMapChange = time.Now()
-		fact.VoidAllVotes() /* Void all votes */
-		fact.WriteVotes()   /* Save to file before exiting */
-		glob.VoteBoxLock.Unlock()
-
-		fact.DoChangeMap(s, fmt.Sprintf("%v", autosaveNum))
-		return
-	}
+	glob.VoteBoxLock.Lock()
+	glob.VoteBox.LastMapChange = time.Now()
+	fact.VoidAllVotes() /* Void all votes */
+	fact.WriteVotes()   /* Save to file before exiting */
+	glob.VoteBoxLock.Unlock()
 
 	fact.ShowMapList(s, i, false)
 }
