1ecb528ebb5f5eb24a8cb9daeb77bd51540cf1df
Distortions81 <carlotto81@gmail.com>
2022-07-29 19:28:07 -0600

    turn skip off if mod resets map

--- commands/moderator/newMapReset.go
+++ commands/moderator/newMapReset.go
@@ -3,11 +3,17 @@ package moderator
 import (
 	"github.com/bwmarrin/discordgo"
 
+	"ChatWire/cfg"
 	"ChatWire/disc"
 	"ChatWire/fact"
 )
 
 func MapReset(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	disc.EphemeralResponse(s, i, "Status:", "Resetting map...")
+
+	//Turn off skip reset flag
+	cfg.Local.Options.SkipReset = false
+	cfg.WriteLCfg()
+
 	go fact.Map_reset("", true)
 }
