8ad13a972d3e3ae626060c77647d4969bc302298
carlotto81 <carlotto81@gmail.com>
2022-12-24 23:32:03 -0700

    version

--- constants/consts.go
+++ constants/consts.go
@@ -3,7 +3,7 @@ package constants
 import "time"
 
 const (
-	Version            = "2588-12.19.2022-1122p"
+	Version            = "2589-12.24.2022-1131p"
 	CWEpoch            = 1653239822390688174
 	SeenDivisor        = 60
 	SeenEpoch          = 1546326000
