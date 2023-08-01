f0db9ac31d4e0ae6edb7da15712094542033e5bd
Distortions81 <carlotto81@gmail.com>
2023-08-01 17:19:17 -0600

    Don't show reset date during warning time, remove 1 hour warning

--- fact/factCron.go
+++ fact/factCron.go
@@ -56,7 +56,7 @@ func doWarn(mins int) {
 }
 
 func InterpSchedule(desc string, test bool) (err bool) {
-	var warn60, warn30, warn15, warn5, warn1, reset string
+	var warn30, warn15, warn5, warn1, reset string
 
 	date := 1
 	dateb := 15
@@ -96,49 +96,42 @@ func InterpSchedule(desc string, test bool) (err bool) {
 	}
 
 	if strings.EqualFold(desc, "three-months") {
-		warn60 = fmt.Sprintf("0 %v %v %v */3 *", minute, hour-1, date)
 		warn30 = fmt.Sprintf("0 %v %v %v */3 *", minute+30, hour-1, date)
 		warn15 = fmt.Sprintf("0 %v %v %v */3 *", minute+45, hour-1, date)
 		warn5 = fmt.Sprintf("0 %v %v %v */3 *", minute+55, hour-1, date)
 		warn1 = fmt.Sprintf("0 %v %v %v */3 *", minute+59, hour-1, date)
 		reset = fmt.Sprintf("0 %v %v %v */3 *", minute, hour, date)
 	} else if strings.EqualFold(desc, "two-months") {
-		warn60 = fmt.Sprintf("0 %v %v %v */2 *", minute+0, hour-1, date)
 		warn30 = fmt.Sprintf("0 %v %v %v */2 *", minute+30, hour-1, date)
 		warn15 = fmt.Sprintf("0 %v %v %v */2 *", minute+45, hour-1, date)
 		warn5 = fmt.Sprintf("0 %v %v %v */2 *", minute+55, hour-1, date)
 		warn1 = fmt.Sprintf("0 %v %v %v */2 *", minute+59, hour-1, date)
 		reset = fmt.Sprintf("0 %v %v %v */2 *", minute, hour, date)
 	} else if strings.EqualFold(desc, "monthly") {
-		warn60 = fmt.Sprintf("0 %v %v %v * *", minute, hour-1, date)
 		warn30 = fmt.Sprintf("0 %v %v %v * *", minute+30, hour-1, date)
 		warn15 = fmt.Sprintf("0 %v %v %v * *", minute+45, hour-1, date)
 		warn5 = fmt.Sprintf("0 %v %v %v * *", minute+55, hour-1, date)
 		warn1 = fmt.Sprintf("0 %v %v %v * *", minute+59, hour-1, date)
 		reset = fmt.Sprintf("0 %v %v %v * *", minute, hour, date)
 	} else if strings.EqualFold(desc, "twice-monthly") {
-		warn60 = fmt.Sprintf("0 %v %v %v,%v * *", minute, hour-1, date, dateb)
 		warn30 = fmt.Sprintf("0 %v %v %v,%v * *", minute+30, hour-1, date, dateb)
 		warn15 = fmt.Sprintf("0 %v %v %v,%v * *", minute+45, hour-1, date, dateb)
 		warn5 = fmt.Sprintf("0 %v %v %v,%v * *", minute+55, hour-1, date, dateb)
 		warn1 = fmt.Sprintf("0 %v %v %v,%v * *", minute+59, hour-1, date, dateb)
 		reset = fmt.Sprintf("0 %v %v %v,%v * *", minute, hour, date, dateb)
 	} else if strings.EqualFold(desc, "fridays") || strings.EqualFold(desc, "day-of-week") {
-		warn60 = fmt.Sprintf("0 %v %v * * %v", minute, hour-1, day)
 		warn30 = fmt.Sprintf("0 %v %v * * %v", minute+30, hour-1, day)
 		warn15 = fmt.Sprintf("0 %v %v * * %v", minute+45, hour-1, day)
 		warn5 = fmt.Sprintf("0 %v %v * * %v", minute+55, hour-1, day)
 		warn1 = fmt.Sprintf("0 %v %v * * %v", minute+59, hour-1, day)
 		reset = fmt.Sprintf("0 %v %v * * %v", minute, hour, day)
 	} else if strings.EqualFold(desc, "odd-dates") {
-		warn60 = fmt.Sprintf("0 %v %v */2 * *", minute, hour-1)
 		warn30 = fmt.Sprintf("0 %v %v */2 * *", minute+30, hour-1)
 		warn15 = fmt.Sprintf("0 %v %v */2 * *", minute+45, hour-1)
 		warn5 = fmt.Sprintf("0 %v %v */2 * *", minute+55, hour-1)
 		warn1 = fmt.Sprintf("0 %v %v */2 * *", minute+59, hour-1)
 		reset = fmt.Sprintf("0 %v %v */2 * *", minute, hour)
 	} else if strings.EqualFold(desc, "daily") {
-		warn60 = fmt.Sprintf("0 %v %v * * *", minute, hour-1)
 		warn30 = fmt.Sprintf("0 %v %v * * *", minute+30, hour-1)
 		warn15 = fmt.Sprintf("0 %v %v * * *", minute+45, hour-1)
 		warn5 = fmt.Sprintf("0 %v %v * * *", minute+55, hour-1)
@@ -152,15 +145,14 @@ func InterpSchedule(desc string, test bool) (err bool) {
 	}
 
 	if !test && reset != "" {
-		err6 := CronVar.AddFunc(warn60, func() { doWarn(60) })
 		err5 := CronVar.AddFunc(warn30, func() { doWarn(30) })
 		err4 := CronVar.AddFunc(warn15, func() { doWarn(15) })
 		err3 := CronVar.AddFunc(warn5, func() { doWarn(5) })
 		err2 := CronVar.AddFunc(warn1, func() { doWarn(1) })
 		err1 := CronVar.AddFunc(reset, func() { go Map_reset("", false) })
 
-		if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil || err6 != nil {
-			cwlog.DoLogCW("interpSchedule: Error adding function: " + err1.Error() + err2.Error() + err3.Error() + err4.Error() + err5.Error() + err6.Error())
+		if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil {
+			cwlog.DoLogCW("interpSchedule: Error adding function: " + err1.Error() + err2.Error() + err3.Error() + err4.Error() + err5.Error())
 			return true
 		} else {
 			return false
@@ -207,20 +199,18 @@ func UpdateScheduleDesc() (err bool) {
 		}
 
 		n := targetEntry.Next
-		if cfg.Local.Options.SkipReset {
-			NextReset = "(SKIP) "
-		} else {
-			NextReset = ""
-		}
-		NextReset = NextReset + n.Format("Mon Jan 02 15:04 MST")
+		NextReset = n.Format("Mon Jan 02 15:04 MST")
 		NextResetUnix = n.Unix()
+		TillReset = durafmt.Parse(time.Until(n).Round(time.Minute)).LimitFirstN(2).Format(units)
+
+		if time.Until(n) < time.Hour {
+			NextReset = "Soon"
+		}
 
 		if cfg.Local.Options.SkipReset {
-			TillReset = "(SKIP) "
-		} else {
-			TillReset = ""
+			TillReset = "(SKIP) " + TillReset
+			NextReset = "(SKIP)" + NextReset
 		}
-		TillReset = TillReset + durafmt.Parse(time.Until(n).Round(time.Minute)).LimitFirstN(2).Format(units)
 
 		return false
 	}
@@ -228,7 +218,7 @@ func UpdateScheduleDesc() (err bool) {
 	//Fallback
 	TillReset = ""
 	NextReset = ""
-	NextResetUnix = 0
+	NextResetUnix = -1
 
 	cwlog.DoLogCW("UpdateScheduleDesc: No schedule set, skipping.")
 	return true
