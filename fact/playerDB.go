57ac29f0fb933e58f113114022fc57bca95069a8
Distortions81 <carlotto81@gmail.com>
2024-07-23 20:01:24 -0600

    .

--- fact/playerDB.go
+++ fact/playerDB.go
@@ -435,7 +435,7 @@ func LoadPlayers(bootMode, minimize bool) {
 				if bootMode {
 					//If primary server, update discord roles
 					if strings.EqualFold(cfg.Local.Callsign, cfg.Global.PrimaryServer) {
-						go AutoPromote(pname, true, false)
+						//go AutoPromote(pname, true, false)
 					}
 					didBan = AddPlayer(pname, tempData[pname].Level, tempData[pname].ID, tempData[pname].Creation, tempData[pname].LastSeen, tempData[pname].BanReason, tempData[pname].SusScore, tempData[pname].Minutes, false)
 				} else if !bootMode {
