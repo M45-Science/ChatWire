5edb52bbc8d0e941c0f7b0d958721d4ae17ee0dc
Distortions81 <carlotto81@gmail.com>
2024-11-01 20:37:18 -0600

    detect deleted messages

--- disc/discUtils.go
+++ disc/discUtils.go
@@ -164,7 +164,7 @@ func SmartEditDiscordEmbed(ch string, msg *discordgo.Message, title, description
 	}
 
 	if DS != nil {
-		if msg != nil && len(msg.Embeds) > 0 {
+		if msg != nil && len(msg.Embeds) > 0 && msg.MessageReference != nil {
 			embed := msg.Embeds[0]
 			embed.Title = title
 			embed.Description = embed.Description + "\n" + description
