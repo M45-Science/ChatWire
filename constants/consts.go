5dbdf689fcc0788e21c70ac0e49f7f2df3626dae
Distortions81 <carlotto81@gmail.com>
2024-11-22 23:19:02 -0700

    version

--- constants/consts.go
+++ constants/consts.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	ProgName           = "ChatWire"
-	Version            = "2652-11-11.2024-0408p"
+	Version            = "2653-11-22.2024-1118p"
 	CWEpoch            = 1653239822390688174
 	SeenDivisor        = 60
 	SeenEpoch          = 1546326000
