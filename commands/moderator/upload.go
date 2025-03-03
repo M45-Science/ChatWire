c932f9a14fc869c145989c6c9d67c2f1ccdeb567
Distortions81 <carlotto81@gmail.com>
2025-03-02 19:34:39 -0700

    start upload command

--- commands/moderator/upload.go
+++ commands/moderator/upload.go
@@ -1,27 +1,40 @@
 package moderator
 
 import (
+	"ChatWire/cfg"
 	"ChatWire/disc"
+	"ChatWire/fact"
 	"ChatWire/factUpdater"
 	"ChatWire/glob"
 	"fmt"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/dustin/go-humanize"
 )
 
 func UploadFile(cmd *glob.CommandData, i *discordgo.InteractionCreate) {
+	factUpdater.FetchLock.Lock()
+	defer factUpdater.FetchLock.Unlock()
+
+	disc.InteractionEphemeralResponse(i, "Status:", "Your file is uploading... please wait.")
+
 	attachmentID := i.ApplicationCommandData().Options[0].Value.(string)
 	attachmentUrl := i.ApplicationCommandData().Resolved.Attachments[attachmentID].URL
-	disc.InteractionEphemeralResponse(i, "Status:", "Your file is uploading... please wait.")
 
 	data, name, err := factUpdater.HttpGet(attachmentUrl)
 	if err != nil {
-		//
+		fact.LogCMS(cfg.Local.Channel.ChatChannel, "File download failed.")
+		return
 	}
-	sBuf := fmt.Sprintf("File: %v, Size: %v", name, humanize.Bytes(uint64(len(data))))
-	disc.DS.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-		Content: "Upload complete: " + sBuf,
-	})
 
+	err = os.WriteFile(name, data, 0555)
+
+	if err != nil {
+		fact.LogCMS(cfg.Local.Channel.ChatChannel, "Unable to write attached file.")
+		return
+	}
+
+	sBuf := fmt.Sprintf("File: %v, Size: %v", name, humanize.Bytes(uint64(len(data))))
+	fact.LogCMS(cfg.Local.Channel.ChatChannel, sBuf)
 }
