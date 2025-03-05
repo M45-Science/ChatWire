73051781870762b7239f4e4dd566721d15ec2ca7
Distortions81 <carlotto81@gmail.com>
2025-03-04 22:16:35 -0700

    get real mod names and versions

---
+++ modupdate/modStruct.go
@@ -0,0 +1,74 @@
+package modupdate
+
+import "time"
+
+type modListData struct {
+	Mods []modData
+}
+type modData struct {
+	Name    string
+	Enabled bool
+}
+
+type modPortalFullData struct {
+	Category          string        `json:"category"`
+	Changelog         string        `json:"changelog"`
+	CreatedAt         time.Time     `json:"created_at"`
+	Description       string        `json:"description"`
+	DownloadsCount    int           `json:"downloads_count"`
+	GithubPath        string        `json:"github_path"`
+	Homepage          string        `json:"homepage"`
+	Images            []ModImages   `json:"images"`
+	LastHighlightedAt string        `json:"last_highlighted_at"`
+	License           ModLicense    `json:"license"`
+	Name              string        `json:"name"`
+	Owner             string        `json:"owner"`
+	Releases          []ModReleases `json:"releases"`
+	Score             float64       `json:"score"`
+	SourceURL         string        `json:"source_url"`
+	Summary           string        `json:"summary"`
+	Tags              []string      `json:"tags"`
+	Thumbnail         string        `json:"thumbnail"`
+	Title             string        `json:"title"`
+	UpdatedAt         time.Time     `json:"updated_at"`
+}
+
+type ModImages struct {
+	ID        string `json:"id"`
+	Thumbnail string `json:"thumbnail"`
+	URL       string `json:"url"`
+}
+
+type ModLicense struct {
+	Description string `json:"description"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+}
+
+type ModInfoJSON struct {
+	Dependencies    []string `json:"dependencies"`
+	FactorioVersion string   `json:"factorio_version"`
+}
+
+type ModReleases struct {
+	DownloadURL string      `json:"download_url"`
+	FileName    string      `json:"file_name"`
+	InfoJSON    ModInfoJSON `json:"info_json"`
+	ReleasedAt  time.Time   `json:"released_at"`
+	Sha1        string      `json:"sha1"`
+	Version     string      `json:"version"`
+}
+
+type modZipInfo struct {
+	Name                string   `json:"name"`
+	Author              string   `json:"author"`
+	Version             string   `json:"version"`
+	Title               string   `json:"title"`
+	Description         string   `json:"description"`
+	Contact             string   `json:"contact"`
+	FactorioVersion     string   `json:"factorio_version"`
+	Dependencies        []string `json:"dependencies"`
+	SpaceTravelRequired bool     `json:"space_travel_required"`
+}
--- modupdate/newModUpdater.go
+++ modupdate/newModUpdater.go
@@ -5,35 +5,130 @@ import (
 	"ChatWire/constants"
 	"ChatWire/cwlog"
 	"ChatWire/util"
+	"archive/zip"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 const modPortalURL = "https://mods.factorio.com/api/mods/%v/full"
 
-type modListData struct {
-	Mods []modData
-}
-type modData struct {
-	Name    string
-	Enabled bool
-}
-
 func CheckModUpdates() {
-	modList, err := GetGameMods()
+	modPath := cfg.Global.Paths.Folders.ServersRoot +
+		cfg.Global.Paths.ChatWirePrefix +
+		cfg.Local.Callsign + "/" +
+		cfg.Global.Paths.Folders.FactorioDir + "/" +
+		constants.ModsFolder + "/"
+
+	//Read mods directory
+	modList, err := os.ReadDir(modPath)
 	if err != nil {
-		cwlog.DoLogCW(err.Error())
+		cwlog.DoLogCW("CheckModUpdates: Unable to read mods dir: " + err.Error())
+		return
 	}
 
-	for _, mod := range modList.Mods {
-		if IsBaseMod(mod.Name) {
-			continue
+	//Find all mods, read info.json inside
+	var fileModList []modZipInfo
+	for _, mod := range modList {
+		if strings.HasSuffix(mod.Name(), ".zip") {
+			modInfo := ReadModZipInfo(mod.Name())
+			if modInfo == nil {
+				continue
+			}
+			fileModList = append(fileModList, *modInfo)
 		}
 	}
 
+	//Read mods-list.json
+	jsonFileList, err := GetGameMods()
+	if err != nil {
+		cwlog.DoLogCW("CheckModUpdates: Unable to mods list: " + err.Error())
+		return
+	}
+
+	//Check both lists, save any that are enabled so we have the mod version + details
+	var finalModList []modZipInfo
+	for _, jmod := range jsonFileList.Mods {
+		if IsBaseMod(jmod.Name) {
+			continue
+		}
+		for _, fmod := range fileModList {
+			if jmod.Name == fmod.Name && jmod.Enabled {
+				finalModList = append(finalModList, fmod)
+			}
+		}
+	}
+
+	//Print out data
+	var buf string
+	for _, item := range finalModList {
+		if buf != "" {
+			buf = buf + ", "
+		}
+		buf = buf + fmt.Sprintf("%v: %v", item.Name, item.Version)
+	}
+
+	if buf == "" {
+		buf = "No enabled game mods found."
+	}
+	cwlog.DoLogCW(buf)
 }
+
+func GetInfoJSONFromZip(zipFilePath string) ([]byte, error) {
+	// Open the zip file
+	zipReader, err := zip.OpenReader(zipFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("GetInfoJSONFromZip:  to open zip file: %w", err)
+	}
+	defer zipReader.Close()
+
+	for _, file := range zipReader.File {
+		if strings.HasSuffix(file.Name, "info.json") {
+			f, err := file.Open()
+			if err != nil {
+				return nil, fmt.Errorf("GetInfoJSONFromZip: failed to open file in zip: %w", err)
+			}
+			defer f.Close()
+
+			var buf bytes.Buffer
+			_, err = io.Copy(&buf, f)
+			if err != nil {
+				return nil, fmt.Errorf("GetInfoJSONFromZip: failed to read file content: %w", err)
+			}
+
+			return buf.Bytes(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("GetInfoJSONFromZip: info.json not found in: " + zipFilePath)
+}
+
+func ReadModZipInfo(modName string) *modZipInfo {
+	path := cfg.Global.Paths.Folders.ServersRoot +
+		cfg.Global.Paths.ChatWirePrefix +
+		cfg.Local.Callsign + "/" +
+		cfg.Global.Paths.Folders.FactorioDir + "/" +
+		cfg.Global.Paths.Folders.Mods + "/" + modName
+
+	data, err := GetInfoJSONFromZip(path)
+	if err != nil {
+		cwlog.DoLogCW("ReadModZipInfo: " + err.Error())
+		return nil
+	}
+
+	modData := modZipInfo{}
+	err = json.Unmarshal(data, &modData)
+	if err != nil {
+		cwlog.DoLogCW("ReadModZipInfo: Unmarshal failure: " + err.Error())
+		return nil
+	}
+
+	return &modData
+}
+
 func IsBaseMod(modName string) bool {
 	if strings.EqualFold(modName, "base") ||
 		strings.EqualFold(modName, "elevated-rails") ||
