1bd503de709e007fdc77a11d5142d8a2d4e6444a
Distortions81 <carlotto81@gmail.com>
2025-03-06 12:28:44 -0700

    add sha256 check

--- factUpdater/fullPackage.go
+++ factUpdater/fullPackage.go
@@ -6,6 +6,9 @@ import (
 	"ChatWire/disc"
 	"ChatWire/fact"
 	"ChatWire/glob"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -15,8 +18,8 @@ import (
 
 const (
 	baseDownloadURL = "https://www.factorio.com/get-download/"
-	baseUpdateURL   = "https://updater.factorio.com/get-download-link?"
 	releaseURL      = "https://factorio.com/api/latest-releases"
+	sha256URL       = "https://www.factorio.com/download/sha256sums/"
 )
 
 func fullPackage(info *InfoData) error {
@@ -37,21 +40,35 @@ func fullPackage(info *InfoData) error {
 	if err != nil {
 		embed := &discordgo.MessageEmbed{Title: "ERROR:", Description: "Downloading Factorio failed."}
 		disc.SmartWriteDiscordEmbed(cfg.Local.Channel.ChatChannel, embed)
-		return fmt.Errorf("failed to get response: %v", err.Error())
+		return errors.New("failed to get response: " + err.Error())
 	}
 
 	cwlog.DoLogCW("Download of %v complete, verifying...", filename)
 	glob.UpdateMessage = disc.SmartEditDiscordEmbed(cfg.Local.Channel.ChatChannel, glob.UpdateMessage, "Updating Factorio", "Verifying update.", glob.COLOR_CYAN)
 
+	hash, err := GetSHA256(filename)
+	if err != nil {
+		emsg := "unable to fetch SHA256 data." + err.Error()
+		cwlog.DoLogCW(emsg)
+		return errors.New(emsg)
+	}
+	if !CheckSHA256(data, hash) {
+		emsg := "the download has an invalid checksum"
+		cwlog.DoLogCW(emsg)
+		return errors.New(emsg)
+	} else {
+		cwlog.DoLogCW("Download SHA256 verified!")
+	}
+
 	archive, err := checkFullPackage(data)
 	if err != nil {
-		return fmt.Errorf("checking full package failed: %v", err.Error())
+		return errors.New("checking full package failed: " + err.Error())
 	}
 
 	pathParts := strings.Split(fact.GetFactorioBinary(), "/")
 	numParts := len(pathParts)
 	if numParts < 4 {
-		return fmt.Errorf("Factorio's binary path does not seem valid. Make sure to include factorio/bin/x64/factorio")
+		return errors.New("factorio's binary path does not seem valid. Make sure to include factorio/bin/x64/factorio")
 	}
 	factPath := strings.Join(pathParts[:numParts-4], "/")
 
@@ -81,26 +98,67 @@ func fullPackage(info *InfoData) error {
 
 	err = untar(factPath, archive)
 	if err != nil {
-		return fmt.Errorf("Installing Factorio to '%v' failed: %v", factPath, err.Error())
+		return fmt.Errorf("installing Factorio to '%v' failed: %v", factPath, err.Error())
 	}
 
 	fact.FactorioVersion = info.Version
 	cwlog.DoLogCW("Factorio was installed to: %v", factPath)
+	glob.UpdateMessage = disc.SmartEditDiscordEmbed(cfg.Local.Channel.ChatChannel, glob.UpdateMessage, "Complete", "Factorio has been installed!", glob.COLOR_CYAN)
 	fact.DoUpdateFactorio = false
 
 	return nil
 }
 
+func GetSHA256(filename string) (string, error) {
+	data, _, err := HttpGet(sha256URL, false)
+	if err != nil {
+		emsg := "Unable to fetch SHA256 sum data: " + err.Error()
+		cwlog.DoLogCW(emsg)
+		return "", errors.New(emsg)
+	}
+	sumsList := strings.Split(string(data), "\n")
+
+	for _, sum := range sumsList {
+		lineParts := strings.Split(sum, "  ")
+		numparts := len(lineParts)
+
+		if numparts == 2 {
+			sum, file := lineParts[0], lineParts[1]
+			if filename == file {
+				return sum, nil
+			}
+		} else {
+			emsg := "SHA256 data is invalid"
+			cwlog.DoLogCW(emsg)
+			return "", errors.New(emsg)
+		}
+	}
+
+	emsg := "SHA256 not found for that file"
+	cwlog.DoLogCW(emsg)
+	return "", errors.New(emsg)
+}
+
+func CheckSHA256(data []byte, checkHash string) bool {
+
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+
+	return strings.EqualFold(hashString, checkHash)
+}
+
 func checkFullPackage(data []byte) ([]byte, error) {
 	archive, err := unXZData(data)
 	if err != nil {
-		return nil, fmt.Errorf("checkFullPackage: xz failed to decompress: %v", err.Error())
+		return nil, errors.New("checkFullPackage: xz failed to decompress:" + err.Error())
 	}
 
 	cwlog.DoLogCW("unxz complete, checking tar file.")
 	err = checkInstallTar(archive)
 	if err != nil {
-		return nil, fmt.Errorf("checkFullPackage: tar verfied failed: %v", err.Error())
+		return nil, errors.New("checkFullPackage: tar verfied failed:" + err.Error())
 	}
 	cwlog.DoLogCW("Install package verified!")
 
--- factUpdater/latest.go
+++ factUpdater/latest.go
@@ -25,8 +25,10 @@ func DoQuickLatest(force bool) (*InfoData, string, bool, bool) {
 		info.VersInt = *newVersion
 		err = fullPackage(info)
 		if err != nil {
+			glob.UpdateMessage = disc.SmartEditDiscordEmbed(cfg.Local.Channel.ChatChannel, glob.UpdateMessage, "Status", "Install failed.", glob.COLOR_CYAN)
 			return info, fmt.Sprintf("DoQuickLatest: fullPackage: %v", err.Error()), true, false
 		}
+		glob.UpdateMessage = disc.SmartEditDiscordEmbed(cfg.Local.Channel.ChatChannel, glob.UpdateMessage, "Complete", "Factorio installed.", glob.COLOR_CYAN)
 		return info, fmt.Sprintf("Factorio %v installed.", newVersion.IntToString()), false, false
 	}
 
@@ -48,6 +50,7 @@ func DoQuickLatest(force bool) (*InfoData, string, bool, bool) {
 		info.VersInt = *newVersion
 		err := fullPackage(info)
 		if err != nil {
+			glob.UpdateMessage = disc.SmartEditDiscordEmbed(cfg.Local.Channel.ChatChannel, glob.UpdateMessage, "Status", "Install failed.", glob.COLOR_CYAN)
 			return info, fmt.Sprintf("DoQuickLatest: fullPackage: %v", err.Error()), true, false
 		}
 
