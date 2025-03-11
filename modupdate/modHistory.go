90662178757ff4324a447d414a80307b75c16f77
Distortions81 <carlotto81@gmail.com>
2025-03-11 03:29:23 -0600

    reformat mod history printout

--- modupdate/modHistory.go
+++ modupdate/modHistory.go
@@ -5,10 +5,18 @@ import (
 )
 
 func ListHistory() string {
+
 	buf := ""
 	for i, item := range ModHistory {
-		buf = buf + fmt.Sprintf("ID#%03v: Name: %v\nVersion: %10vnDate: %v\n",
-			i, item.Name, item.Version, item.Date)
+		buf = buf + fmt.Sprintf("%-5v: %v\n",
+			i+1, item.Name)
+		if item.Notes != "" {
+			buf = buf + "Notes: \n" + item.Notes
+		}
+		if item.Version != "" {
+			buf = buf + "Version: \n" + item.Version
+		}
+		buf = buf + item.Date.Format("01-02-2006 03:04 PM")
 	}
 	if buf == "" {
 		buf = "History is empty."
