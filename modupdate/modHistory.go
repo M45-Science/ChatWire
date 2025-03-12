ec28cd858c863a93f1d0c9c9ae6e3c35188d5b36
Distortions81 <carlotto81@gmail.com>
2025-03-12 02:00:32 -0600

    fix mod history formatting

--- modupdate/modHistory.go
+++ modupdate/modHistory.go
@@ -11,12 +11,12 @@ func ListHistory() string {
 		buf = buf + fmt.Sprintf("%-5v: %v\n",
 			i+1, item.Name)
 		if item.Notes != "" {
-			buf = buf + "Notes: \n" + item.Notes
+			buf = buf + "Notes: " + item.Notes + "\n"
 		}
 		if item.Version != "" {
-			buf = buf + "Version: \n" + item.Version
+			buf = buf + "Version: " + item.Version + "\n"
 		}
-		buf = buf + item.Date.Format("01-02-2006 03:04 PM") + "\n\n"
+		buf = buf + item.Date.UTC().Format("01-02-2006 03:04 PM") + " UTC\n\n"
 	}
 	if buf == "" {
 		buf = "History is empty."
