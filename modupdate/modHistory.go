3a29f46266d58ea26e3faa2a5f5be629fd0dd00b
Distortions81 <carlotto81@gmail.com>
2025-03-15 10:41:00 -0600

    progress

--- modupdate/modHistory.go
+++ modupdate/modHistory.go
@@ -8,48 +8,30 @@ const maxItemsPage = 25
 
 func ListHistory() string {
 
-	bufa := ""
+	buf := ""
 	var histCount = 0
 	for i, item := range ModHistory.History {
 		histCount++
 
-		bufa = bufa + fmt.Sprintf("%-5v: %v\n",
+		buf = buf + fmt.Sprintf("%-5v: %v\n",
 			i+1, item.Name)
 		if item.Notes != "" {
-			bufa = bufa + "Notes: " + item.Notes + "\n"
+			buf = buf + "Notes: " + item.Notes + "\n"
 		}
 		if item.Version != "" {
-			bufa = bufa + "Version: " + item.Version + "\n"
+			buf = buf + "Version: " + item.Version + "\n"
 		}
-		bufa = bufa + item.Date.UTC().Format("01-02-2006 03:04 PM") + " UTC\n\n"
+		buf = buf + item.Date.UTC().Format("01-02-2006 03:04 PM") + " UTC\n\n"
 
 		if histCount >= maxItemsPage {
 			break
 		}
 	}
-	if bufa == "" {
-		bufa = "Mod history is empty."
-	}
-	bufb := ""
-	var blackCount = 0
-	for i, item := range ModHistory.Blacklist {
-		bufb = bufb + fmt.Sprintf("%-5v: %v\n",
-			i+1, item.Name)
-		if item.Notes != "" {
-			bufb = bufb + "Notes: " + item.Notes + "\n"
-		}
-		if item.Version != "" {
-			bufb = bufb + "Version: " + item.Version + "\n"
-		}
-		bufb = bufb + item.Date.UTC().Format("01-02-2006 03:04 PM") + " UTC\n\n"
-
-		blackCount++
-	}
-	if bufb == "" {
-		bufb = "Updater blacklist is empty."
+	if buf == "" {
+		buf = "Mod history is empty."
 	}
 
-	return bufa + "\n" + bufb + "\n"
+	return buf
 }
 
 func ClearHistory() string {
