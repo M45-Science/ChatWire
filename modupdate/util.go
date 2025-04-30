aeac00a465327c53389045a8d845c08f2eba6646
Distortions81 <carlotto81@gmail.com>
2025-04-30 08:21:09 -0600

    handle no mod-list json, so we can disable space age mods

--- modupdate/util.go
+++ modupdate/util.go
@@ -400,11 +400,23 @@ func MergeModLists(modFileList []modZipInfo, jsonModList ModListData) []modZipIn
 		}
 	}
 
-	/*
-		for _, item := range installedMods {
-			cwlog.DoLogCW("mergeModLists: %v-%v", item.Name, item.Version)
+	baseMods := []string{"base", "elevated-rails", "quality", "space-age"}
+	for _, bmod := range baseMods {
+		found := false
+		for _, mod := range installedMods {
+			if mod.Name == bmod {
+				found = true
+				break
+			}
 		}
-	*/
+		if !found {
+			if bmod == "base" {
+				installedMods = append(installedMods, modZipInfo{Name: bmod, Enabled: true})
+			} else {
+				installedMods = append(installedMods, modZipInfo{Name: bmod, Enabled: false})
+			}
+		}
+	}
 
 	return installedMods
 }
