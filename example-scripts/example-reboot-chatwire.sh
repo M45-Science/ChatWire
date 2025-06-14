6444526c2153a196954d5acb6d4e478a641d7d4d
Distortions81 <carlotto81@gmail.com>
2025-06-14 00:47:20 -0600

    add bash-check

--- example-scripts/example-reboot-chatwire.sh
+++ example-scripts/example-reboot-chatwire.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! "$BASH_VERSION" ] ; then
+        echo "Not launched with bash, re-running with bash."
+        bash build.sh
+    exit 1
+fi
+
 USER_NAME=$(whoami)
 
 for letter in {a..r}; do
