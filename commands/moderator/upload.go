f15cc8c6a045f48e726c152839c8902c0f96608d
Distortions81 <carlotto81@gmail.com>
2025-11-14 10:10:21 -0700

    fix upload command bug.

--- commands/moderator/upload.go
+++ commands/moderator/upload.go
@@ -16,6 +16,10 @@ func UploadFile(cmd *glob.CommandData, i *discordgo.InteractionCreate) {
 	uploadLock.Lock()
 	defer uploadLock.Unlock()
 
+	foundOption = false
+	foundSave = false
+	foundModList = false
+
 	//Save current auto-mod-update setting, disable mod updating, then restore on exit.
 	RestoreSetting := cfg.Local.Options.ModUpdate
 	cfg.Local.Options.ModUpdate = false
